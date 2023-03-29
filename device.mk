6a9cc17e83424b09301b5f62a00262cc69577ef3
Chenyang Zhong <zhongcy95@gmail.com>
2023-03-29 04:57:42 +0000

    raphael: Build and enable fastbootd mode Change-Id: I7f4246f3f583dcae7b3bfc0d3ae1cf4d577632cb Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.raphael \
